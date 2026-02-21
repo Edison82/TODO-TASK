74819c2a07974f98c35b316bbe79f6015a8c7d02
Edison_Liberato <edison@gmail.com>
2026-02-20 23:20:48 -0500

    Todo terminado

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const api = require('./api');
+var path = require("path");
 const port = process.env.PORT || 3000;
 const app = express();
+
+
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 app.listen(port, function () {
     console.log("Server is listening at port: " + port);
 });
 
 app.get('/', function (req, res) {
-    res.send("hello world");
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+app.use('/api', api);
